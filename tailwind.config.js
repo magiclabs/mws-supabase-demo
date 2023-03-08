1364aa197fcdc0fb452ef776fada137ff5c529c7
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2023-03-08 12:20:09 -0800

    adds brand styles to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,20 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", ...fontFamily.sans]
+      },
+      colors: {
+        "brand-purple": "#6851ff",
+        "brand-purple-text": "#522FD4",
+        "brand-purple-light": "#EDEBFF",
+        "brand-ink": "#18171A",
+      },
+      boxShadow: {
+        brand: "8 8px 48px rgba(24, 23, 26, .2)",
+      }
+    },
   },
   plugins: [],
 }
