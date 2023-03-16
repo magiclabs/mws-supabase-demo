65d206a308865455df62bf14ac7b5844d84346ff
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2023-03-16 12:07:28 -0700

    adds login link

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <div>
       <h1>Supabase with Magic Wallet Services</h1>
+      <Link href="/login">Sign in</Link>
     </div>
   );
 }
